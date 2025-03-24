8a761626ecfcf6eb720e3cba794400af6667a783
Teama Amer <145798330+teamaamer@users.noreply.github.com>
2025-03-24 22:39:59 +0200

    Delete backend directory

--- backend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect, authorizeRoles } = require('../middleware/authMiddleware');
-
-router.get('/dashboard', protect, authorizeRoles('admin'), (req, res) => {
-  res.json({ message: "Welcome to the admin dashboard!" });
-});
-
-module.exports = router;
